f7cf7a4a0af14be7b096cd8ea9a2d3b6e088c195
DashinIN <green19grant@yandex.ru>
2023-07-05 17:39:41 +0300

    jest config and start testing

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,35 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only first param', () => {
+        expect(classNames('someClass')).toBe('someClass ');
+    });
+    test('with additional class', () => {
+        const expected = 'someClass class1 class2 ';
+        expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected);
+    });
+    test('with modes class', () => {
+        const expected = 'someClass class1 class2 hovered';
+        expect(classNames(
+            'someClass',
+            { hovered: true },
+            ['class1', 'class2'],
+        )).toBe(expected);
+    });
+    test('with modes class', () => {
+        const expected = 'someClass class1 class2 ';
+        expect(classNames(
+            'someClass',
+            { hovered: false },
+            ['class1', 'class2'],
+        )).toBe(expected);
+    });
+    test('with modes class', () => {
+        const expected = 'someClass class1 class2 ';
+        expect(classNames(
+            'someClass',
+            { hovered: undefined },
+            ['class1', 'class2'],
+        )).toBe(expected);
+    });
+});
