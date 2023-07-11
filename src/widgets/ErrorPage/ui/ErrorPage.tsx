9e5d40922c06561f520bb75c2c7296281cee5725
DashinIN <green19grant@yandex.ru>
2023-07-11 14:33:28 +0300

    error boundary added

---
+++ src/widgets/ErrorPage/ui/ErrorPage.tsx
@@ -0,0 +1,23 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
+import s from './ErrorPage.module.scss';
+
+interface ErrorPageProps {
+ className?: string;
+}
+
+export const ErrorPage = ({ className }: ErrorPageProps) => {
+    const { t } = useTranslation();
+
+    const reloadPage = () => {
+        // eslint-disable-next-line
+        location.reload();
+    };
+    return (
+        <div className={classNames(s.ErrorPage, {}, [className])}>
+            <p>{t('Произошла непредвиденная ошибка')}</p>
+            <Button onClick={reloadPage}>{t('Обновить страницу')}</Button>
+        </div>
+    );
+};
