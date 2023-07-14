317032514d620ddcbe2f4f0dc021677f5e455905
DashinIN <green19grant@yandex.ru>
2023-07-14 16:20:32 +0300

    sidebar icons added

--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -6,6 +6,8 @@ import { useTranslation } from 'react-i18next';
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import MainIcon from 'shared/assets/icons/main-20-20.svg';
+import AboutIcon from 'shared/assets/icons/about-20-20.svg';
 import s from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -40,17 +42,22 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 <AppLink
                     theme={AppLinkTheme.SECONDARY}
                     to={RoutePath.main}
-                    className={s.link}
+                    className={s.item}
                 >
-                    {t('Главная страница')}
+                    <MainIcon className={s.icon} />
+                    <span className={s.link}>
+                        {t('Главная страница')}
+                    </span>
                 </AppLink>
-
                 <AppLink
                     theme={AppLinkTheme.SECONDARY}
                     to={RoutePath.about}
-                    className={s.link}
+                    className={s.item}
                 >
-                    {t('О сайте')}
+                    <AboutIcon className={s.icon} />
+                    <span className={s.link}>
+                        {t('О сайте')}
+                    </span>
                 </AppLink>
             </div>
             <div className={s.switchers}>
