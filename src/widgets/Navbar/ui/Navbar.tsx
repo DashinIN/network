b0dad6a9d08a92c4b390d296b50fc055e66768bf
DashinIN <green19grant@yandex.ru>
2023-07-14 16:36:50 +0300

    fix tests sidebar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,4 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { useTranslation } from 'react-i18next';
 import s from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -8,11 +6,6 @@ interface NavbarProps {
 
 }
 
-export const Navbar = ({ className }: NavbarProps) => {
-    const { t } = useTranslation();
-    return (
-        <div className={classNames(s.Navbar, {}, [className])}>
-            <div className={s.links} />
-        </div>
-    );
-};
+export const Navbar = ({ className }: NavbarProps) => (
+    <div className={classNames(s.Navbar, {}, [className])} />
+);
