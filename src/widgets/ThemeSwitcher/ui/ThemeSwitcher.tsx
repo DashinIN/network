5dcecd8d6a59a7df2cd76d4b88eb42f00266fec0
DashinIN <green19grant@yandex.ru>
2023-07-26 14:21:34 +0300

    pink theme added

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -2,6 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
+import PinkIcon from 'shared/assets/icons/theme-pink.svg';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { memo } from 'react';
 
@@ -18,7 +19,10 @@ export const ThemeSwitcher = memo(({ className }: ThemeSwitcherProps) => {
             onClick={toggleTheme}
             theme={ButtonTheme.CLEAR}
         >
-            { theme === Theme.LIGHT ? <LightIcon /> : <DarkIcon />}
+            {/* eslint-disable-next-line no-nested-ternary */}
+            { theme === Theme.LIGHT ? <LightIcon />
+                : theme === Theme.DARK ? <DarkIcon />
+                    : <PinkIcon />}
         </Button>
     );
 });
