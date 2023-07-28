f7d309c31c99d38f78ad8ab76fa6f8a86b3bd8f7
DashinIN <green19grant@yandex.ru>
2023-07-28 09:39:52 +0300

    protected routes add

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -9,3 +9,7 @@ export {
 export {
     getUserAuthData,
 } from './model/selectors/getUserAuthData/getUserAuthData';
+
+export {
+    getUserInited,
+} from './model/selectors/getUserInited/getUserInited';
