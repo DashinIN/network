0359a56a7213ac6f3afddfe303c082be53abbc56
DashinIN <green19grant@yandex.ru>
2023-08-30 15:44:24 +0300

    h ui selector

--- src/entities/Currency/ui/CurrencySelect/CurrencySelect.tsx
+++ src/entities/Currency/ui/CurrencySelect/CurrencySelect.tsx
@@ -2,6 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { Select } from 'shared/ui/Select/Select';
 import { memo, useCallback } from 'react';
+import { ListBox } from 'shared/ui/ListBox/ListBox';
 import { Currency } from '../../model/types/currency';
 
 interface CurrencySelectProps {
@@ -27,13 +28,23 @@ export const CurrencySelect = memo(({
     }, [onChange]);
 
     return (
-        <Select
+        // <Select
+        //     className={classNames('', {}, [className])}
+        //     label={t('Укажите валюту')}
+        //     options={options}
+        //     value={value}
+        //     onChange={onChangeHandler}
+        //     readonly={readonly}
+        // />
+        <ListBox
             className={classNames('', {}, [className])}
-            label={t('Укажите валюту')}
-            options={options}
-            value={value}
             onChange={onChangeHandler}
+            value={value}
+            defaultValue={t('Укажите валюту')}
+            label={t('Укажите валюту')}
+            items={options}
             readonly={readonly}
+            direction="top"
         />
     );
 });
