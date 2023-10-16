666076b4c92e5c2864651ef684f5073f80dbbae0
DashinIN <green19grant@yandex.ru>
2023-10-16 18:38:51 +0300

    add no-unused-imports plugin to eslint config and fix versions conflict

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -1,5 +1,5 @@
 import { Link, LinkProps } from 'react-router-dom';
-import { FC, ReactNode, memo } from 'react';
+import { ReactNode, memo } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import s from './AppLink.module.scss';
 
